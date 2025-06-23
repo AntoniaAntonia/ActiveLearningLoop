859cda00850996d1527d1090f68df6d0730dff62
AntoniaAntonia <160243137+AntoniaAntonia@users.noreply.github.com>
2025-06-23 12:42:24 +0200

    Create run_loop.py

---
+++ run_loop.py
@@ -0,0 +1,54 @@
+from active_learning_loop_test import ActiveLearningLoop
+import os
+import re
+import sys
+import time
+import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from itertools import combinations_with_replacement, product
+from sklearn.model_selection import KFold, train_test_split
+from ase import Atoms, io
+from ase.build import molecule
+from ase.calculators.emt import EMT
+from ase.data import covalent_radii, atomic_numbers
+from ase.io import read, write, Trajectory
+from ase.neighborlist import NeighborList, natural_cutoffs
+from mace.calculators import MACECalculator
+from mace.cli.run_train import run
+from mace.tools import build_default_arg_parser
+
+# === User-defined setup ===
+elements = ["C", "O", "Pt"]
+r_max = 10.0
+cell_size = r_max * 2 + 10.0
+total_structures = 3000
+dataset_name = "dimers_1000"
+nanoparticle_path = "MD_300_43_F_opt_final.xyz"
+df_r_min_per_pair = pd.read_csv("/home/cat/s233070/Loop/class_1/data/info/r_min_per_pair.csv")
+
+loop = ActiveLearningLoop(
+    elements=elements,
+    r_max=r_max,
+    cell_size=cell_size,
+    total_structures=total_structures,
+    dataset_name=dataset_name,
+    df_r_min_per_pair=df_r_min_per_pair,
+    nanoparticle_path=nanoparticle_path,
+    n_points=167,
+    max_complexity=8,
+    start_from=2,
+    top_k = 30,
+    mode="start",
+    start_model_name="dimers_1000_complexity_2"
+)
+
+
+# === Run the loop ===
+loop.run()
+
+
+
+# dimer number reduced (I set n_points to 50) 
+# log file created
